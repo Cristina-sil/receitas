3420f2f79fea03693eb6ec76e33ab24e9ed5ded1
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-01-05 22:31:11 -0300

    Exemplo de template

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps criados que precisaram ser adicionados manualmente
+    'recipes',
 ]
 
 MIDDLEWARE = [
