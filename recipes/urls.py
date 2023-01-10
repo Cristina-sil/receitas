993757e81f900615f8ab2f085bdacb59415ddec8
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-01-10 20:52:52 -0300

    Uma nova URL para uma nova View

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from recipes.views import home
+from . import \
+    views  # como o arquivo está na mesma pasta, foi utilizado o ponto, ao invés de from recipes.view import home
 
 urlpatterns = [
-    path('', home),  # HOME
+    path('', views.home),  # HOME
+    path('recipes/<int:id>/', views.recipes),
+
 ]
