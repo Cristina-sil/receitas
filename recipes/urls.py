cc38a246d931ae91c0efa1373ef1247cd4d490fe
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-01-13 22:06:08 -0300

    Criando os models de Recipes e Category, e efetuando a migração

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+# recipes:recipe, ou recipes:home
+
 urlpatterns = [
-    path('', views.home),  # HOME
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name='home'),  # HOME
+    path('recipes/<int:id>/', views.recipes,
+         name='recipe'),  # detail page
 
 ]
