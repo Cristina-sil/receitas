eabb1e4529dad67d3bdb5fc719798ec8b745173a
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-01-26 16:48:52 -0300

    Ocultando receitas que não estão publicadas e alterando o template de categorias

--- recipes/views.py
+++ recipes/views.py
@@ -9,7 +9,9 @@ from .models import Recipe
 # Create your views here.
 def home(request):
     # Para importar todas as receitas, ordenadas utilizando o método de order_by
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -17,9 +19,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
