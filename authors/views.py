94b9df32ff191742263a51b5901b20d2d2f2b1c1
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-02-13 16:58:21 -0300

    Início da criação da view de create para o form de authors usando o SESSIONS para salvar os valores de request

--- authors/views.py
+++ authors/views.py
@@ -5,10 +5,21 @@ from .forms import RegisterForm
 
 # Create your views here.
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    request.session['number'] = request.session.get('number') or 1
+    request.session['number'] += 1
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
+
+
+'''raise levanta um erro'''
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    form = RegisterForm(request.POST)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
