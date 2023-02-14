b5b17db6c32740fb37768b23275a28fbaab5eb5d
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-02-14 00:06:38 -0300

    Ajustes

--- authors/views.py
+++ authors/views.py
@@ -5,21 +5,17 @@ from .forms import RegisterForm
 
 # Create your views here.
 def register_view(request):
-    request.session['number'] = request.session.get('number') or 1
-    request.session['number'] += 1
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
-
-
-'''raise levanta um erro'''
 
 
 def register_create(request):
     if not request.POST:
         raise Http404()
-    form = RegisterForm(request.POST)
-    return render(request, 'authors/pages/register_view.html', {
-        'form': form,
-    })
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+    return redirect('authors:register')
